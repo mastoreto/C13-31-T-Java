75d27e383a60199f4b98f47c318ac813b39bc915
Santiago Rodriguez <05rmsantiago@gmail.com>
2023-08-23 00:01:49 -0300

    chore(backend): :wrench: configuración para vscode

--- backend/src/main/java/com/c1331tjava/ServiceApp/ServiceAppApplication.java
+++ backend/src/main/java/com/c1331tjava/ServiceApp/ServiceAppApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAppApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
