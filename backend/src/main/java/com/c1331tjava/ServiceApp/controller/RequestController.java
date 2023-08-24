bd5f185246d85e6d700107b719c9a211b621ec8c
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-08-24 15:21:22 -0300

    feat(backend): Trabajando en endpoint de requests

--- backend/src/main/java/com/c1331tjava/ServiceApp/controller/RequestController.java
+++ backend/src/main/java/com/c1331tjava/ServiceApp/controller/RequestController.java
@@ -1,13 +1,35 @@
 package com.c1331tjava.ServiceApp.controller;
 
+import com.c1331tjava.ServiceApp.model.Request;
+import com.c1331tjava.ServiceApp.model.UserEntity;
 import com.c1331tjava.ServiceApp.service.RequestService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/request")
 @AllArgsConstructor
 public class RequestController {
 
     RequestService requestService;
+
+    @GetMapping("/list/page/{page}")
+    public Page<Request> findByClientAndActive(@PathVariable int page, Pageable pageable){
+
+        Pageable twoPerPage = PageRequest.of(page, 2);
+
+        // Buscar userEntity con el usuario sacado del token
+        UserEntity userEntity = new UserEntity();
+
+        return this.requestService.findByClientAndActiveTrue(userEntity, twoPerPage);
+
+    }
 
 }
--- backend/src/main/java/com/c1331tjava/ServiceApp/service/RequestService.java
+++ backend/src/main/java/com/c1331tjava/ServiceApp/service/RequestService.java
@@ -1,14 +1,25 @@
 package com.c1331tjava.ServiceApp.service;
 
 import com.c1331tjava.ServiceApp.model.Request;
+import com.c1331tjava.ServiceApp.model.UserEntity;
 import com.c1331tjava.ServiceApp.repository.RequestRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class RequestService {
 
     RequestRepository requestRepository;
+
+    public Page<Request> findByClientAndActiveTrue(UserEntity userEntity, Pageable pageable){
+
+        return this.requestRepository.findByClientAndActiveTrue(userEntity, pageable);
+
+    }
 
 }
