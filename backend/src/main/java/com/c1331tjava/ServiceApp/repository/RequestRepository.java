b65f7fd3b6efa17f41129d02c255d6c86a5763d8
JEB <jebius76@gmail.com>
2023-09-11 18:56:52 -0300

    feat(backend): Se agrego endpoint para prestador para consulta de tareas nuevas.

--- backend/src/main/java/com/c1331tjava/ServiceApp/repository/RequestRepository.java
+++ backend/src/main/java/com/c1331tjava/ServiceApp/repository/RequestRepository.java
@@ -3,6 +3,7 @@ package com.c1331tjava.ServiceApp.repository;
 import com.c1331tjava.ServiceApp.model.Bid;
 import com.c1331tjava.ServiceApp.model.Request;
 import com.c1331tjava.ServiceApp.model.UserEntity;
+import com.c1331tjava.ServiceApp.model.Zone;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,5 +19,7 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
     Page<Request> findByClientAndActiveTrue(UserEntity userEntity, Pageable pageable);
 
     Page<Request> findByProvidersContaining(UserEntity userEntity, Pageable pageable);
-
+    Page<Request> findByDescriptionContainingAndEndedFalseOrderByDate(String criteria, Pageable pageable);
+    Page<Request> findByZoneAndEndedFalseOrderByDate(Zone zone, Pageable pageable);
+    Page<Request> findByDescriptionContainingAndZoneAndEndedFalseOrderByDate(String criteria, Zone zone, Pageable pageable);
 }
